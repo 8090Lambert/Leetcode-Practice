fe6feddd429406f2c384efd721f6769d9f7a8cf9
8090Lambert <404060692@qq.com>
2019-04-01 16:23:33 +0800

    mySqrt

--- golang/mySqrt/mySqrt.go
+++ golang/mySqrt/mySqrt.go
@@ -1,13 +1,10 @@
 package mySqrt
 
-import "fmt"
-
 func MySqrt(x int) int {
 	result := x
 	for result > x / result {
 		result = (result +  x / result) / 2
 	}
 
-	fmt.Println(result)
 	return result
 }
