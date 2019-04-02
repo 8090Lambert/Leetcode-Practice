de08a184cd5e34ab12afc7545ccc74198a705499
8090Lambert <404060692@qq.com>
2019-04-02 20:26:47 +0800

    deleteDuplicates

---
+++ golang/deleteDuplicates/deleteDuplicates.go
@@ -0,0 +1,18 @@
+package deleteDuplicates
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func DeleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	for current != nil && current.Next != nil {
+		if current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+	return head
+}
