ba69820a9fb52eb586f2ea42cc4fd2898c1aca2a
8090Lambert <404060692@qq.com>
2019-04-10 19:37:39 +0800

    generate

---
+++ golang/generate/generate.go
@@ -0,0 +1,20 @@
+package generate
+
+func Generate(numRows int) [][]int {
+	res := make([][]int, 0)
+
+	for i := 0; i < numRows; i++ {
+		tmp := make([]int, 0)
+		for j := 0; j <= i; j++ {
+			if j == 0 || j == i {
+				tmp = append(tmp, 1)
+			} else {
+				need := res[i-1][j-1] + res[i-1][j]
+				tmp = append(tmp, need)
+			}
+		}
+		res = append(res, tmp)
+	}
+
+	return res
+}
