d8186bbb77765fb4dadaa09ed810560174e0da83
8090Lambert <404060692@qq.com>
2019-04-18 20:33:38 +0800

    findMedianSortedArrays

---
+++ medium/findMedianSortedArrays/findMedianSortedArrays.go
@@ -0,0 +1,16 @@
+package findMedianSortedArrays
+
+func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	count1, count2 := len(nums1), len(nums2)
+	sum := count1 + count2
+	var ret float64
+	if sum%2 == 0 {
+		if count1 < sum/2 {
+
+		} else {
+
+		}
+	} else {
+
+	}
+}
