13496a7923c0b4c03f16caa38bdddaa494b232a7
8090Lambert <404060692@qq.com>
2019-05-23 14:56:54 +0800

    combinationSum

--- medium/combinationSum/combinationSum.go
+++ medium/combinationSum/combinationSum.go
@@ -2,6 +2,7 @@ package combinationSum
 
 import (
 	"sort"
+	"strconv"
 )
 
 func CombinationSum(candidates []int, target int) [][]int {
@@ -24,6 +25,45 @@ func backtrack(candidates []int, target, i int, tmp []int) [][]int {
 		allTmp := backtrack(candidates, target-candidates[index], index, temp)
 		if allTmp != nil {
 			res = append(res, allTmp...)
+		}
+	}
+
+	return res
+}
+
+func CombinationSum2(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	uniqueMap := map[string]bool{}
+	collect := find2(candidates, 0, target, []int{}, uniqueMap)
+	return collect
+}
+
+func find2(candidates []int, start, target int, tmp []int, uniqueMap map[string]bool) [][]int {
+	if target == 0 {
+		var str string
+		for _, value := range tmp {
+			str = str + strconv.Itoa(value) + ":"
+		}
+		if _, ok := uniqueMap[str]; !ok {
+			uniqueMap[str] = true
+			return [][]int{tmp}
+		} else {
+			return nil
+		}
+	}
+
+	if target < 0 {
+		return nil
+	}
+
+	res := [][]int{}
+	for index := start; index < len(candidates); index++ {
+		temp := []int{}
+		temp = append(temp, tmp...)
+		temp = append(temp, candidates[index])
+		newRes := find2(candidates, index+1, target-candidates[index], temp, uniqueMap)
+		if newRes != nil {
+			res = append(res, newRes...)
 		}
 	}
 
