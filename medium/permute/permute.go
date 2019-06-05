1f87b2664f78e2a591ea1cc30b5709368a377304
8090Lambert <404060692@qq.com>
2019-06-05 15:15:40 +0800

    permuteUnique

--- medium/permute/permute.go
+++ medium/permute/permute.go
@@ -1,26 +1,48 @@
 package permute
 
-var res [][]int
+import "fmt"
+
+//var res [][]int
 
 func Permute(nums []int) [][]int {
+	res := [][]int{}
 	if len(nums) <= 1 {
 		return [][]int{nums}
 	}
-	var backTrack func(left, ans []int)
-	backTrack = func(left, ans []int) {
-		if len(left) == 0 {
-			res = append(res, ans)
-			return
-		}
+	// 方法一
+	backTrack1(nums, []int{}, &res)
 
-		for key, value := range left {
-			cleft := append([]int{}, left...)
-			cans := append([]int{}, ans...)
-
-			backTrack(append(cleft[:key], cleft[key+1:]...), append(cans, value))
-		}
-	}
-	backTrack(nums, []int{})
-
+	// 方法二
+	//backTrack2(nums, &res, 0)
+	fmt.Println(res)
 	return res
+}
+
+func backTrack1 (left, ans []int, res *[][]int) {
+	if len(left) == 0 {
+		tmp := append([]int{}, ans...)
+		*res = append(*res, tmp)
+		return
+	}
+
+	for key, value := range left {
+		cleft := append([]int{}, left...)
+		cans := append([]int{}, ans...)
+
+		backTrack1(append(cleft[:key], cleft[key+1:]...), append(cans, value), res)
+	}
+}
+
+func backTrack2 (nums []int, res *[][]int, start int) {
+	if len(nums) == start {
+		item := append([]int{}, nums...)
+		*res = append(*res, item)
+		return
+	}
+
+	for i := start; i < len(nums); i++ {
+		nums[i], nums[start] = nums[start], nums[i]
+		backTrack2(nums, res, start+1)
+		nums[start], nums[i] = nums[i], nums[start]
+	}
 }
---
+++ medium/permuteUnique/permuteUnique.go
@@ -0,0 +1,27 @@
+package permuteUnique
+
+func PermuteUnique(nums []int) [][]int {
+	res := [][]int{}
+	backTrack(nums, &res, 0)
+
+	return res
+}
+
+func backTrack(nums []int, res *[][]int, start int) {
+	if len(nums) == start {
+		tmp := append([]int{}, nums...)
+		*res = append(*res, tmp)
+		return
+	}
+
+	unique := map[int]bool{}
+	for i := start; i < len(nums); i++ {
+		if _, ok := unique[nums[i]]; ok {
+			continue
+		}
+		nums[i], nums[start] = nums[start], nums[i]
+		backTrack(nums, res, start+1)
+		nums[start], nums[i] = nums[i], nums[start]
+		unique[nums[i]] = true
+	}
+}
