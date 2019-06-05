1ead718f9e22aabc768da5a5d7e05d4c25c804b0
8090Lambert <404060692@qq.com>
2019-06-05 18:28:55 +0800

    spiralOrder

---
+++ medium/spiralOrder/spiralOrder.go
@@ -0,0 +1,35 @@
+package spiralOrder
+
+func SpiralOrder(matrix [][]int) []int {
+	res := make([]int, 0)
+	if len(matrix) <= 0 {
+		return res
+	}
+
+	rowStart, colStart := 0, 0
+	rowEnd, colEnd := len(matrix)-1, len(matrix[0])-1
+
+	for rowStart <= rowEnd && colStart <= colEnd {
+		for c := colStart; c <= colEnd; c++ {
+			res = append(res, matrix[rowStart][c])
+		}
+		for r := rowStart + 1; r <= rowEnd; r++{
+			res = append(res, matrix[r][colEnd])
+		}
+
+		if rowStart < rowEnd && colStart < colEnd {
+			for c := colEnd-1; c > colStart; c--{
+				res = append(res, matrix[rowEnd][c])
+			}
+
+			for r := rowEnd; r > rowStart; r-- {
+				res = append(res, matrix[r][colStart])
+			}
+		}
+
+		colStart++
+		colEnd--
+		rowStart++
+		rowEnd--
+	}
+}
