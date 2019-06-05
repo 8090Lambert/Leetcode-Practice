a41c279fb7d9bb7406b238b20db1fc06814c1ac3
8090Lambert <404060692@qq.com>
2019-06-05 17:10:09 +0800

    mypow

---
+++ medium/myPow/myPow.go
@@ -0,0 +1,21 @@
+package myPow
+
+func myPow(x float64, n int) float64 {
+	N := n
+	if N < 0 {
+		x = 1/x
+		N = -N
+	}
+
+	var res float64 = 1.0
+	var current float64 = x
+	for i := N; i > 0; i /= 2 {
+		if i % 2 == 1 {
+			res = res * current
+		}
+
+		current = current * current
+	}
+
+	return res
+}
