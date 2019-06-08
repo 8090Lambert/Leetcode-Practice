f44f2e8f197680319b973ef8db118cb14d91ebdb
8090Lambert <juzs215@gmail.com>
2019-06-08 16:18:05 +0800

    merge

--- medium/merge/merge.go
+++ medium/merge/merge.go
@@ -1,10 +1,41 @@
 package merge
 
-func merge (intervals [][]int) [][]int {
+import (
+	"sort"
+)
+
+func Merge (intervals [][]int) [][]int {
+	if len(intervals) <= 0 {
+		return [][]int{}
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
 	res := [][]int{intervals[0]}
 	count := len(intervals)
-	compare := 0
 	for i := 1; i < count; i++ {
-
+		if intervals[i][0] <= res[len(res)-1][0] {
+			if intervals[i][1] <= res[len(res)-1][1] {
+				if intervals[i][1] > res[len(res)-1][0] {
+					res[len(res)-1][0] = intervals[i][0]
+				} else {
+					tmp := [][]int{intervals[i]}
+					res = append(tmp, res...)
+				}
+			} else if intervals[i][1] > res[len(res)-1][1] {
+				res[len(res)-1][0] = intervals[i][0]
+				res[len(res)-1][1] = intervals[i][1]
+			}
+		} else {
+			if intervals[i][0] > res[len(res)-1][1] {
+				res = append(res, intervals[i])
+			} else {
+				if intervals[i][1] > res[len(res)-1][1] {
+					res[len(res)-1][1] = intervals[i][1]
+				}
+			}
+		}
 	}
+	
+	return res
 }
