b4f8d673bda21f1a737d25cac0ae571f1e3336c8
8090Lambert <404060692@qq.com>
2019-06-11 18:02:10 +0800

    setZeros

---
+++ medium/setZeroes/setZeroes.go
@@ -0,0 +1,25 @@
+package setZeroes
+
+func setZeroes(matrix [][]int)  {
+	row, col := len(matrix), len(matrix[0])
+	rowSet := map[int]int{}
+	colSet := map[int]int{}
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if matrix[i][j] == 0 {
+				rowSet[i] = i
+				colSet[j] = j
+			}
+		}
+	}
+
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if _, rok := rowSet[i]; rok{
+				matrix[i][j] = 0
+			} else if _, cok := colSet[j]; cok{
+				matrix[i][j] = 0
+			}
+		}
+	}
+}
