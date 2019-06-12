edea00dbc31329196fcbbfc309c04d8b5a31f286
8090Lambert <404060692@qq.com>
2019-06-12 15:37:39 +0800

    sortColors

---
+++ medium/sortColors/sortColors.go
@@ -0,0 +1,18 @@
+package sortColors
+
+func sortColors(nums []int)  {
+	current, second, third := 0, 0, len(nums)-1
+
+	for current <= third {
+		if nums[current] == 0 {
+			nums[second],nums[current] = nums[current], nums[second]
+			current++
+			second++
+		} else if nums[current] == 2 {
+			nums[third],nums[current] = nums[current], nums[third]
+			third--
+		} else {
+			current++
+		}
+	}
+}
