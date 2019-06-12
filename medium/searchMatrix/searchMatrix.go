d07a0c77786533772b20663b10c98ef9fb716216
8090Lambert <404060692@qq.com>
2019-06-12 15:25:49 +0800

    searchMatrix

---
+++ medium/searchMatrix/searchMatrix.go
@@ -0,0 +1,23 @@
+package searchMatrix
+
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	row, col := len(matrix), len(matrix[0])
+	left, right := 0, row * col - 1
+
+
+	for left <= right {
+		idx := (left + right) / 2
+		if target == matrix[idx/col][idx%col] {
+			return true
+		} else if target < matrix[idx/col][idx%col] {
+			right = idx - 1
+		} else {
+			left = idx + 1
+		}
+	}
+
+	return false
+}
