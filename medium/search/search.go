f4ca71a33648b3adffc24b319001409f5d32decd
8090Lambert <404060692@qq.com>
2019-06-13 19:56:09 +0800

    medium/search

--- medium/search/search.go
+++ medium/search/search.go
@@ -57,3 +57,37 @@ func searchLoop(nums []int, left, right, target int) int {
 
 	return -1
 }
+
+func search81 (nums []int, target int) bool {
+	count := len(nums)
+	if count == 0 {
+		return false
+	}
+
+	left := 0
+	right := count - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return true
+		}
+		if nums[mid] == nums[right] && nums[mid] == nums[left] {
+			left++
+			right--
+		} else if nums[mid] <= nums[right] {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			if nums[mid] > target && target >= nums[left] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
+	}
+
+	return false
+}
