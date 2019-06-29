2f912b6c558f2ae50b0bb218352be09a2058fd51
8090Lambert <juzs215@gmail.com>
2019-06-29 15:54:52 +0800

    invertTree

---
+++ easy/golang/invertTree/invertTree.go
@@ -0,0 +1,38 @@
+package invertTree
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil || root.Right == nil && root.Left == nil{
+		return root
+	}
+	
+	root.Left = invertTree(root.Right)
+	root.Right = invertTree(root.Left)
+	
+	return root
+}
+
+func invertTreeWithLoop (root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	list := []*TreeNode{root}
+	for len(list) != 0 {
+		current := list[0]
+		current.Left, current.Right = current.Right, current.Left
+		list = append([]*TreeNode{}, list[1:]...)
+		if current.Left != nil {
+			list = append(list, current.Left)
+		}
+		if current.Right != nil {
+			list = append(list, current.Right)
+		}
+	}
+	
+	return root
+}
