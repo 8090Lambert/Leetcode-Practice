b08d84ea000ae1557f133cb9e07d2f05b7f2fe47
8090Lambert <juzs215@gmail.com>
2019-07-05 22:23:35 +0800

    easy

---
+++ easy/golang/deleteNode/deleteNode.go
@@ -0,0 +1,11 @@
+package deleteNode
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func deleteNode (node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
---
+++ easy/golang/lowestCommonAncestor/lowestCommonAncestor.go
@@ -0,0 +1,47 @@
+package lowestCommonAncestor
+
+type TreeNode struct {
+	Val int
+	Left *ListNode
+	Right *ListNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	pVal := p.Val
+	qVal := q.Val
+	
+	node := root
+	for node != nil {
+		current := node.Val
+		if pVal > current && qVal > current {
+			node = node.Right
+		} else if pVal < current && qVal < current {
+			node = node.Left
+		} else {
+			return node
+		}
+	}
+	
+	return nil
+}
+
+func lowestCommonAncestor1 (root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	
+	pVal := p.Val
+	qVal := q.Val
+	current := root.Val
+	
+	if pVal > current && qVal > current {
+		return lowestCommonAncestor1(root.Right, p, q)
+	} else if pVal < current && qVal < current {
+		return lowestCommonAncestor1(root.Left, p, q)
+	} else {
+		return root
+	}
+	
+	return nil
+}
+
