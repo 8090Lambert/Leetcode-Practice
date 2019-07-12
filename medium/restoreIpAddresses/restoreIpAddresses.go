51d7835f749a9efd20ab1bf86cfe03a1c09ee171
8090Lambert <404060692@qq.com>
2019-07-12 11:07:00 +0800

    restoreIpAddresses

--- medium/restoreIpAddresses/restoreIpAddresses.go
+++ medium/restoreIpAddresses/restoreIpAddresses.go
@@ -1,10 +1,40 @@
 package restoreIpAddresses
 
-func restoreIpAddresses (s string) []string {
+import "strconv"
 
+func restoreIpAddresses (s string) []string {
+	collect := make([]string, 0)
+	backTrack(s, 0, "", &collect)
+
+	return collect
 }
 
 
-func backTrack () {
+func backTrack (s string, start int, tmp string, res *[]string) {
+	if start > 4 {
+		return
+	}
 
+	if start == 4 && len(s) == 0 {
+		*res = append(*res, tmp)
+		return
+	}
+
+	for i := 1; i < 4; i++ {
+		if len(s) < i {
+			break
+		}
+		sub := s[:i]
+		val, _ := strconv.Atoi(sub)
+		if val > 255 || len(sub) > 1 && string(sub[0]) == "0" {
+			continue
+		}
+
+		temp := tmp+sub
+		if start != 3 {
+			temp = tmp+sub+"."
+		}
+
+		backTrack(s[i:], start+1, temp, res)
+	}
 }
