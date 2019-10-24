7904473492e715259d5530935821d146f73ffc86
8090Lambert <404060692@qq.com>
2019-10-24 20:11:44 +0800

    isSameTree

--- easy/golang/isSameTree/isSameTree.go
+++ easy/golang/isSameTree/isSameTree.go
@@ -18,16 +18,51 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	}
 }
 
-
-func isSameTree1(p *TreeNode, q *TreeNode) bool {
+func isSameTreeLoop(p *TreeNode, q *TreeNode) bool {
 	if p == nil && q == nil {
 		return true
 	}
-
-
-	if p != nil && q != nil && p.Val == q.Val {
-		return isSameTree1(q.Left, p.Left) && isSameTree1(q.Right, p.Right)
-	} else {
+	if p == nil || q == nil || p.Val != q.Val {
 		return false
 	}
+	stackp := []*TreeNode{p}
+	stackq := []*TreeNode{q}
+	for len(stackp) > 0 {
+		p := stackp[0]
+		q := stackq[0]
+		stackp = stackp[1:]
+		stackq = stackq[1:]
+		if !check(p, q) {
+			return false
+		}
+		if p != nil {
+			if !check(p.Left, q.Left) || !check(p.Right, q.Right){
+				return false
+			}
+			if p.Left != nil {
+				stackp = append(stackp, p.Left)
+				stackq = append(stackq, q.Left)
+			}
+			if p.Right != nil {
+				stackp = append(stackp, p.Right)
+				stackq = append(stackq, q.Right)
+			}
+		}
+	}
+	return true
+}
+
+func check (p, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return true
 }
