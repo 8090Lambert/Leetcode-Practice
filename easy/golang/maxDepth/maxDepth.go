dd44cef24a170c1a12ca3db2250b49605e1cf6ea
8090Lambert <404060692@qq.com>
2019-10-29 11:56:31 +0800

    maxDepth

--- easy/golang/maxDepth/maxDepth.go
+++ easy/golang/maxDepth/maxDepth.go
@@ -17,3 +17,27 @@ func MaxDepth(root *TreeNode) int {
 		return int(math.Max(float64(MaxDepth(root.Left)), float64(MaxDepth(root.Right)))) + depth
 	}
 }
+
+// DFS
+func maxDepth(root *TreeNode) int {
+	stack := make([]*TreeNode, 0)
+	if root != nil {
+		stack = append(stack, root)
+	}
+	depth := 0
+	for len(stack) > 0 {
+		depth += 1
+		count := len(stack)
+		for i := 0; i < count; i++ {
+			if stack[i].Left != nil {
+				stack = append(stack, stack[i].Left)
+			}
+			if stack[i].Right != nil {
+				stack = append(stack, stack[i].Right)
+			}
+		}
+		stack = append([]*TreeNode{}, stack[count:]...)
+	}
+
+	return depth
+}
