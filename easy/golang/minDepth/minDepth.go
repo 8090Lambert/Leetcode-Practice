b5c5e12a18c6d476f93744fc0e0eecc937c603ad
8090Lambert <juzs215@gmail.com>
2019-10-30 23:25:21 +0800

    minDepth

--- easy/golang/minDepth/minDepth.go
+++ easy/golang/minDepth/minDepth.go
@@ -23,3 +23,18 @@ func minDepth(root *TreeNode) int {
 		}
 	}
 }
+
+func minDepthDFS(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	
+	if root.Left != nil && root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	if root.Right != nil && root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	
+	return int(math.Min(float64(minDepth(root.Left)), float64(minDepth(root.Right)))) + 1
+}
