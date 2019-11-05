6595f8b6c810af78b26e10b621da90959a6ce4af
8090Lambert <404060692@qq.com>
2019-11-05 18:46:46 +0800

    hasPathsum

--- easy/golang/hasPathSum/hasPathSum.go
+++ easy/golang/hasPathSum/hasPathSum.go
@@ -17,3 +17,31 @@ func hasPathSum(root *TreeNode, sum int) bool {
 
 	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
 }
+
+
+func hasPathSum1(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	stack := []*TreeNode{root}
+	total := []int{sum}
+	for len(stack) > 0 {
+		count := len(stack)
+		current := stack[count-1]
+		stack = append([]*TreeNode{}, stack[:count-1]...)
+		target := total[len(total)-1]
+		total = append([]int{}, total[:len(total)-1]...)
+		if target == current.Val && current.Left == nil && current.Right == nil {
+			return true
+		}
+		if current.Left != nil {
+			total = append(total, target - current.Val)
+			stack = append(stack, current.Left)
+		}
+		if current.Right != nil {
+			total = append(total, target - current.Val)
+			stack = append(stack, current.Right)
+		}
+	}
+	return false
+}
