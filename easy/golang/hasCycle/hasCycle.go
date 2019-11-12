334c89094a1eb7a53758f67349ae913564b7214d
8090Lambert <404060692@qq.com>
2019-11-12 21:15:27 +0800

    hasCycle

--- easy/golang/hasCycle/hasCycle.go
+++ easy/golang/hasCycle/hasCycle.go
@@ -19,3 +19,34 @@ func HasCycle(head *ListNode) bool {
 	}
 	return true
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow := head
+	fast := head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return true
+}
