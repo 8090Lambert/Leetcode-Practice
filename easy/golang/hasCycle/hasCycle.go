d09f4e2e8ec8ffc02972c2077dcb61dbc87653a0
8090Lambert <404060692@qq.com>
2019-12-03 20:21:33 +0800

    hasCycle

--- easy/golang/hasCycle/hasCycle.go
+++ easy/golang/hasCycle/hasCycle.go
@@ -5,48 +5,18 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func HasCycle(head *ListNode) bool {
-	if head == nil || head.Next == nil {
-		return false
-	}
-	slow, fast := head, head.Next
-	for slow != fast {
-		if fast == nil || fast.Next == nil {
-			return false
-		}
-		slow = slow.Next
-		fast = fast.Next.Next
-	}
-	return true
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 func hasCycle(head *ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
 	}
 	slow := head
-	fast := head.Next
-	for slow != fast {
-		if fast == nil || fast.Next == nil {
-			return false
-		}
+	fast := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
 		slow = slow.Next
-		fast = fast.Next
+		if fast == slow {
+			return true
+		}
 	}
-	return true
+	return false
 }
