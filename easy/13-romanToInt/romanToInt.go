3640831d4749722d0bd855ae534158bdc5cde6bf
8090Lambert <404060692@qq.com>
2021-07-29 23:06:49 +0800

    roman int

--- easy/13-romanToInt/romanToInt.go
+++ easy/13-romanToInt/romanToInt.go
@@ -1,8 +1,7 @@
 package _3_romanToInt
 
-
 func romanToInt(s string) int {
-	mapper := map[byte]int{
+	m := map[byte]int{
 		'I': 1,
 		'V': 5,
 		'X': 10,
@@ -11,15 +10,37 @@ func romanToInt(s string) int {
 		'D': 500,
 		'M': 1000,
 	}
-	res, prev, current := 0, 0, 0
-	for i := 0; i < len(s); i++ {
-		current = mapper[s[i]]
-		if current > prev {
-			res += current
+
+	res, count := 0, len(s)
+	for i := range s {
+		current := m[s[i]]
+		if i < count-1 && current < m[s[i+1]] {
+			res -= current
 		} else {
-			res = res + current - prev * 2
+			res += current
 		}
-		prev = current
+	}
+	return res
+}
+
+
+func romanToInt1(s string) int {
+	m := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	res, prev := 0, 0
+	for i := 0; i < len(s); i++ {
+		res += m[s[i]]
+		if m[s[i]] > prev {
+			res -= 2 * prev
+		}
+		prev = m[s[i]]
 	}
 	return res
 }
