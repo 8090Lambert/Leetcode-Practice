5c45b0d3946bfd36f21e3834fb21a073f6c74eaa
8090Lambert <404060692@qq.com>
2021-08-03 22:43:40 +0800

    merge arr

--- easy/88-merge/merge.go
+++ easy/88-merge/merge.go
@@ -17,3 +17,21 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 		sum--
 	}
 }
+
+func mergeArr(nums1 []int, m int, nums2 []int, n int) {
+	sum := m + n
+	for m > 0 && n > 0 {
+		if nums1[m-1] > nums2[n-1] {
+			nums1[sum-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[sum-1] = nums2[n-1]
+			n--
+		}
+		sum--
+	}
+	for i := n-1; i >= 0; i-- {
+		nums1[sum-1] = nums2[n]
+		sum--
+	}
+}
