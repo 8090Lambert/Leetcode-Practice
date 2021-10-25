406098a58253f6ccc74690b85b54c63a148ffb93
8090Lambert <404060692@qq.com>
2021-10-25 21:53:24 +0800

    searchRange

--- medium/34-searchRange/searchRange.go
+++ medium/34-searchRange/searchRange.go
@@ -40,31 +40,24 @@ func SearchRange(nums []int, target int) []int {
 }
 
 func searchRange(nums []int, target int) []int {
-	start, end, position := 0, len(nums)-1, -1
-	left, right := 0, 0
-	for start <= end {
-		mid := start + (end - start) >> 1
+	start, end := -1, -1
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
 		if nums[mid] == target {
-			position = mid
-			left, right = mid, mid
-			for true {
-				if left - 1 >= 0 && nums[left-1] == target {
-					left--
-				} else if right + 1 < len(nums) && nums[right+1] == target {
-					right++
-				} else {
-					break
-				}
+			start, end = mid, mid
+			for start-1 >= 0 && nums[start] == nums[start-1] {
+				start--
+			}
+			for end+1 < len(nums) && nums[end] == nums[end+1] {
+				end++
 			}
 			break
-		} else if nums[mid] < target {
-			start = mid + 1
+		} else if target < nums[mid] {
+			right = mid-1
 		} else {
-			end = mid - 1
+			left = mid+1
 		}
 	}
-	if position == -1 {
-		return []int{-1, -1}
-	}
-	return []int{left, right}
+	return []int{start, end}
 }
