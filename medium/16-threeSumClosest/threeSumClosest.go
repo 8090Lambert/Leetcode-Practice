e613e7be6dbbf71c2c85c6afc695896aba6fb8d5
8090Lambert <404060692@qq.com>
2021-10-26 11:31:50 +0800

    threeSumClosest

--- medium/16-threeSumClosest/threeSumClosest.go
+++ medium/16-threeSumClosest/threeSumClosest.go
@@ -5,39 +5,6 @@ import (
 	"sort"
 )
 
-func ThreeSumClosest(nums []int, target int) int {
-	sort.Ints(nums)
-	count := len(nums)
-	if count < 3 {
-		return 0
-	}
-	if count == 3 {
-		return nums[0] + nums[1] + nums[2]
-	}
-	close := nums[0] + nums[1] + nums[2]
-	for index := 0; index < count-1; index++ {
-		left := index + 1
-		right := count - 1
-		for left < right {
-			sum := nums[index] + nums[left] + nums[right]
-			if math.Abs(float64(target-sum)) < math.Abs(float64(target-close)) {
-				close = sum
-			}
-			if sum == target {
-				return target
-			} else if sum > target{
-				right--
-			} else {
-				left++
-			}
-		}
-	}
-
-	return close
-}
-
-
-
 func threeSumClosest1(nums []int, target int) int {
 	sort.Ints(nums)
 	count := len(nums)
@@ -53,6 +20,36 @@ func threeSumClosest1(nums []int, target int) int {
 		for left < right {
 			sum := nums[i] + nums[left] + nums[right]
 			if math.Abs(float64(target-sum)) < math.Abs(float64(target-res)) {
+				res = sum
+			}
+			if sum == target {
+				return target
+			} else if sum > target {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+	return res
+}
+
+func ThreeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	res := 0
+	for i := 0; i < 3; i++ {
+		res += nums[i]
+	}
+	count := len(nums)
+	for i := 0; i < count && count > 3; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left := i+1
+		right := count-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if int(math.Abs(float64(target-sum))) < int(math.Abs(float64(target-res))) {
 				res = sum
 			}
 			if sum == target {
