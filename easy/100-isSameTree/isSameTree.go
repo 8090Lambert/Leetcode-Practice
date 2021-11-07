b36b745dfa701ab8d4fabdee2d2d70f1d9811df5
8090Lambert <404060692@qq.com>
2021-11-07 21:00:21 +0800

    issame

--- easy/100-isSameTree/isSameTree.go
+++ easy/100-isSameTree/isSameTree.go
@@ -18,6 +18,31 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	}
 }
 
+func isSameTreeDfS(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+	stack := []*TreeNode{p, q}
+	for len(stack) != 0 {
+		count := len(stack)
+		l, r := stack[count-1], stack[count-2]
+		stack = stack[:count-2]
+		if !compare(l, r) || !compare(l.Left, r.Left) || !compare(l.Right, r.Right) {
+			return false
+		}
+		if l.Left != nil {
+			stack = append(stack, l.Left, r.Left)
+		}
+		if r.Right != nil {
+			stack = append(stack, l.Right, r.Right)
+		}
+	}
+	return len(stack) == 0
+}
+
 func isSameTreeLoop(p *TreeNode, q *TreeNode) bool {
 	if p == nil && q == nil {
 		return true
@@ -32,7 +57,7 @@ func isSameTreeLoop(p *TreeNode, q *TreeNode) bool {
 		q := stackq[0]
 		stackp = stackp[1:]
 		stackq = stackq[1:]
-		if !check(p, q) || !check(p.Left, q.Left) || !check(p.Right, q.Right) {
+		if !compare(p, q) || !compare(p.Left, q.Left) || !compare(p.Right, q.Right) {
 			return false
 		}
 		if p != nil {
@@ -49,7 +74,7 @@ func isSameTreeLoop(p *TreeNode, q *TreeNode) bool {
 	return true
 }
 
-func check (p, q *TreeNode) bool {
+func compare(p, q *TreeNode) bool {
 	if p == nil && q == nil {
 		return true
 	}
@@ -62,17 +87,4 @@ func check (p, q *TreeNode) bool {
 	}
 
 	return true
-}
-
-
-func isSameTreeOther(p *TreeNode, q *TreeNode) bool {
-	if p == nil && q == nil {
-		return true
-	}
-
-	if p != nil && q != nil && q.Val == p.Val {
-		return isSameTreeOther(q.Left, p.Left) && isSameTreeOther(q.Right, p.Right)
-	} else {
-		return false
-	}
 }
