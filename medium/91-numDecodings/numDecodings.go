918304317781808000b40642a9e2dceea4c39782
such <such@suchdeMacBook-Pro.local>
2021-12-22 11:25:58 +0800

    numdecoding

--- medium/91-numDecodings/numDecodings.go
+++ medium/91-numDecodings/numDecodings.go
@@ -87,3 +87,24 @@ func numDecodings1(s string) int {
 	}
 	return cur
 }
+
+
+func numDecodingsDP(s string) int {
+	if s[0] == '0' {
+		return 0
+	}
+	pre, cur := 1, 1
+	for i := 1; i < len(s); i++ {
+		tmp := cur
+		if s[i] == '0' {
+			if s[i-1] != '1' && s[i-1] != '2' {
+				return 0
+			}
+			cur = pre
+		} else if s[i] <= '6' && (s[i-1] == '1' || s[i-1] == '2') || s[i] > '6' && s[i-1] == '1' {
+			cur += pre
+		}
+		pre = tmp
+	}
+	return cur
+}
