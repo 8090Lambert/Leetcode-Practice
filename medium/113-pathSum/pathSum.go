85861e1cc91e2b2c24621fc0188cf9cac1a8a667
8090Lambert <juzs215@gmail.com>
2021-12-25 22:21:55 +0800

    pathSum

--- medium/113-pathSum/pathSum.go
+++ medium/113-pathSum/pathSum.go
@@ -27,3 +27,22 @@ func backTrack(root *TreeNode, target, current int, path []int, res *[][]int) {
 	backTrack(root.Right, target, current, path, res)
 	path = path[:len(path)-1]
 }
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	res := make([][]int, 0)
+	backTrack1(root, targetSum, []int{}, &res)
+	return res
+}
+
+func backTrack1(root *TreeNode, targetSum int, item []int, res *[][]int) {
+	if targetSum < 0 || root == nil {
+		return
+	}
+	item = append(item, root.Val)
+	if root.Val == targetSum && root.Left == nil && root.Right == nil {
+		*res = append(*res, item)
+		return
+	}
+	backTrack1(root.Left, targetSum-root.Val, item, res)
+	backTrack1(root.Right, targetSum-root.Val, item, res)
+}
